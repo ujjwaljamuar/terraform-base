07b0981d4cc234c4b0ee7988150e6c455e2e65cf
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-10 21:10:39 +0530

    variables in terraform, types

--- main.tf
+++ main.tf
@@ -2,13 +2,27 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+variable "dev_vpc_cidr" {
+  description = "dev vpc cidr range"
+  default = "10.0.0.0/16"
+  type = string // bool, string, number, list(string), list(object({cidr=string,name:string}))
+}
+
+variable "pvt_subnet_1_cidr" {
+  description = "private subnet cidr range"
+}
+
+variable "pub_subnet_1_cidr" {
+  description = "public subnet cidr range"
+}
+
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.dev_vpc_cidr
 }
 
 resource "aws_subnet" "pvt_subnet_1" {
   vpc_id            = aws_vpc.dev_vpc.id
-  cidr_block        = "10.0.10.0/24"
+  cidr_block        = var.pvt_subnet_1_cidr
   availability_zone = "ap-south-1a"
 }
 
@@ -23,7 +37,7 @@ data "aws_vpc" "default_vpc" {
 
 resource "aws_subnet" "pub_subnet_1" {
   vpc_id            = data.aws_vpc.default_vpc.id
-  cidr_block        = "172.31.48.0/20"
+  cidr_block        = var.pub_subnet_1_cidr
   availability_zone = "ap-south-1b"
 }
 
