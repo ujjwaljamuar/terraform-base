2495fafb2f550c50d5788b969fe4c6950b0697df
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-04-19 23:57:09 +0530

    added support for using default route table and associated subnet to route table

--- project1/main.tf
+++ project1/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
+variable "region" {}
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avl_zone" {}
@@ -33,6 +34,20 @@ resource "aws_internet_gateway" "myapp_igw" {
   }
 }
 
+/*
+resource "aws_default_route_table" "default_rtb" {
+  default_route_table_id = aws_vpc.myapp_vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp_igw.id
+  }
+
+  tags = {
+    Name : "${var.env}-default-rtb"
+  }
+}
+*/
+
 resource "aws_route_table" "myapp_route_table" {
   vpc_id = aws_vpc.myapp_vpc.id
   route {
@@ -43,4 +58,9 @@ resource "aws_route_table" "myapp_route_table" {
   tags = {
     Name : "${var.env}-rtb"
   }
+}
+
+resource "aws_route_table_association" "myapp_rtb_assc" {
+  subnet_id      = aws_subnet.myapp_subnet_1.id
+  route_table_id = aws_route_table.myapp_route_table.id
 }
