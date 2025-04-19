aea8515ee1b84e6b91dfb4ef43e739a32f8601dd
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-04-19 23:34:14 +0530

    created internet gatewat and route table

--- project1/main.tf
+++ project1/main.tf
@@ -9,16 +9,38 @@ variable "env" {}
 
 resource "aws_vpc" "myapp_vpc" {
   cidr_block = var.vpc_cidr_block
+
   tags = {
     Name : "${var.env}-vpc"
   }
 }
 
-resource "aws_subnet" "my_app_subnet_1" {
+resource "aws_subnet" "myapp_subnet_1" {
   vpc_id            = aws_vpc.myapp_vpc.id
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avl_zone
+
   tags = {
     Name : "${var.env}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "myapp_igw" {
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  tags = {
+    Name : "${var.env}-igw"
+  }
+}
+
+resource "aws_route_table" "myapp_route_table" {
+  vpc_id = aws_vpc.myapp_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp_igw.id
+  }
+
+  tags = {
+    Name : "${var.env}-rtb"
   }
 }
